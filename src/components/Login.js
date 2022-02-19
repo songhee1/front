3e83c466bb135fef35c5cfd7f1d39d3773a17ad5
unknown <ghenrhkwk88@naver.com>
2022-02-20 01:08:37 +0900

    First Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,7 @@
 import React,{Component} from 'react';
-import './Login.css';
 
 class Login extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      userID:"",
-      userPW:"",
-      nickname:"",
-      isLogin:null
-    };
-  }
+  
   //인풋 아이디값 
   inputID=(e)=>{
      this.setState({userID: e.target.value});
@@ -30,40 +21,47 @@ class Login extends Component{
       headers:{
         'Content-type':'application/json'
       },
+      //로그인 key와 value 값
       body:JSON.stringify({
-      id:this.state.userID,
-      pw:this.state.userPW
-    })//문자열로 전달 
+      id:this.props.userid,
+      pw:this.props.userpw
+    }) 
   })
     .then(response=>response.json())
     .then(response=>{
-      if(response.token){
-         localStorage.setItem("wtw-token",response.token);
+      if(response.ACCESS_TOKEN){
+         localStorage.setItem("login-token",response.ACCESS_TOKEN);
          this.props.history.push("/main_gh");
 
-      }else if(!response.token){
+      }else if(!response.ACCESS_TOKEN){
         alert("회원이 아닙니다");
+        //Login 컴포넌트 그대로 위치
         this.props.history.push("/signup_gh");
       }
     });
   }
-    render(){
-
+  render(){
         return(
           <div class="login_total" >
           {/* 로그인 폼 */}
-          <form class="login_total_foam"
+           <form  
            onSubmit={this.submitHandler} >
            <label><h2>Login</h2></label>
-           <input type="text" name="id"  placeholder='아이디'
-             onChange={this.inputID}  
-             value={this.state.userID} />            
-           <input type="password" name="password"  placeholder='비밀번호' 
-             onChange={this.inputPW}
-             value={this.state.userPW}/> 
+           <input 
+           type="text" 
+           name="id"  
+           placeholder='아이디'
+           onChange={this.inputID}  
+           value={this.state.userID} />            
+           <input 
+           type="password" 
+           name="password"  
+           placeholder='비밀번호' 
+           onChange={this.inputPW}
+            value={this.state.userPW}/> 
+
           {/* 로그인 버튼 */}
            <button type="submit" href="/">로그인</button>
-           
           </form>
         
           {/* 하단 바 */}
@@ -71,7 +69,7 @@ class Login extends Component{
            <button href="/pwfind">비밀번호 찾기</button>
            아직 계정이 없으신가요? <button >회원가입</button>
          </div>
-        )
+        );
       }
     }
 
